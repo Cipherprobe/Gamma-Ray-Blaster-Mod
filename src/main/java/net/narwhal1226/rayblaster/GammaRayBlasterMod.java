740f2f63be6b21391fd7d7d04213bbb72f9bd06f
Cipherprobe <clayton2007@mvla.us>
2022-12-17 12:46:17 -0800

    Added ingredients, new tab, updated desc

--- src/main/java/net/narwhal1226/rayblaster/GammaRayBlasterMod.java
+++ src/main/java/net/narwhal1226/rayblaster/GammaRayBlasterMod.java
@@ -1,14 +1,21 @@
 package net.narwhal1226.rayblaster;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.*;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -17,14 +24,51 @@ public class GammaRayBlasterMod
 {
     public static final String MOD_ID = "rayblaster";
     private static final Logger LOGGER = LogUtils.getLogger();
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GammaRayBlasterMod.MOD_ID);
+
+    public static final RegistryObject<Item> NUCLEGON = ITEMS.register("nuclegon",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_NUCLEGON = ITEMS.register("raw_nuclegon",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> EXCITED_URANIUM = ITEMS.register("excited_uranium",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_URANIUM = ITEMS.register("raw_uranium",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 
     public GammaRayBlasterMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ITEMS.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::buildContents);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void buildContents(CreativeModeTabEvent.Register event) {
+        event.registerCreativeModeTab(new ResourceLocation(MOD_ID, "gammaraytab"), builder ->
+                // Set name of tab to display
+                builder.title(Component.translatable("item_group." + MOD_ID + ".rayblaster"))
+                        // Set icon of creative tab
+                        .icon(() -> new ItemStack(NUCLEGON.get()))
+                        // Add default items to tab
+                        .displayItems((enabledFlags, populator, hasPermissions) -> {
+                            populator.accept(NUCLEGON.get());
+                            populator.accept(RAW_NUCLEGON.get());
+                            populator.accept(EXCITED_URANIUM.get());
+                            populator.accept(RAW_URANIUM.get());
+                        })
+        );
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
