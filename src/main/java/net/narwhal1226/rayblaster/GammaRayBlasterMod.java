03cd55e2557d365ffc049ea3916d2927768ee30a
Cipherprobe <clayton2007@mvla.us>
2022-12-18 19:31:44 -0800

    Added uranium, ores, and unique tab

--- src/main/java/net/narwhal1226/rayblaster/GammaRayBlasterMod.java
+++ src/main/java/net/narwhal1226/rayblaster/GammaRayBlasterMod.java
@@ -3,7 +3,12 @@ package net.narwhal1226.rayblaster;
 import com.mojang.logging.LogUtils;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
@@ -18,6 +23,9 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
+import java.rmi.registry.Registry;
+import java.util.function.Supplier;
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(GammaRayBlasterMod.MOD_ID)
 public class GammaRayBlasterMod
@@ -25,6 +33,7 @@ public class GammaRayBlasterMod
     public static final String MOD_ID = "rayblaster";
     private static final Logger LOGGER = LogUtils.getLogger();
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GammaRayBlasterMod.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GammaRayBlasterMod.MOD_ID);
 
     public static final RegistryObject<Item> NUCLEGON = ITEMS.register("nuclegon",
             () -> new Item(new Item.Properties()));
@@ -35,7 +44,27 @@ public class GammaRayBlasterMod
     public static final RegistryObject<Item> RAW_URANIUM = ITEMS.register("raw_uranium",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Block> NUCLEGON_ORE = registerBlock("nuclegon_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f)
+            .requiresCorrectToolForDrops(), UniformInt.of(6, 9)));
 
+    public static final RegistryObject<Block> URANIUM_ORE = registerBlock("uranium_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f)
+            .requiresCorrectToolForDrops(), UniformInt.of(9, 11)));
+
+    public static final RegistryObject<Block> DEEPSLATE_NUCLEGON_ORE = registerBlock("deepslate_nuclegon_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f)
+            .requiresCorrectToolForDrops(), UniformInt.of(6, 9)));
+
+    public static final RegistryObject<Block> DEEPSLATE_URANIUM_ORE = registerBlock("deepslate_uranium_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f)
+            .requiresCorrectToolForDrops(), UniformInt.of(9, 11)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static  <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
@@ -46,6 +75,7 @@ public class GammaRayBlasterMod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ITEMS.register(modEventBus);
+        BLOCKS.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::buildContents);
@@ -67,6 +97,10 @@ public class GammaRayBlasterMod
                             populator.accept(RAW_NUCLEGON.get());
                             populator.accept(EXCITED_URANIUM.get());
                             populator.accept(RAW_URANIUM.get());
+                            populator.accept(NUCLEGON_ORE.get());
+                            populator.accept(DEEPSLATE_NUCLEGON_ORE.get());
+                            populator.accept(URANIUM_ORE.get());
+                            populator.accept(DEEPSLATE_URANIUM_ORE.get());
                         })
         );
     }
